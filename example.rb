3aa7fc025280c5e843e6012cc9a21175d939d640
barseek <sergey.b@hey.com>
2022-07-28 14:17:04 +0200

    Uncomment example

--- example.rb
+++ example.rb
@@ -1,111 +1,111 @@
 require_relative "lib/statinize"
 
-# Statinize::Statinizer.configure do |config|
-#   # Every class by default will raise an error
-#   config.force = true
-# end
+Statinize::Statinizer.configure do |config|
+  # Every class by default will raise an error
+  config.force = true
+end
 
-# class ExampleClass
-#   include Statinize::Statinizable
+class ExampleClass
+  include Statinize::Statinizable
 
-#   statinize do
-#     # Won't raise an error
-#     force false
-#     attribute :first_name, :last_name, type: String
-#     validate :first_name, inclusion: ["haha"], force: true, if: -> { last_name == "b" }
-#     validate :first_name, inclusion: ["hehe"], if: -> { last_name == "a" }
+  statinize do
+    # Won't raise an error
+    force false
+    attribute :first_name, :last_name, type: String
+    validate :first_name, inclusion: ["haha"], force: true, if: -> { last_name == "b" }
+    validate :first_name, inclusion: ["hehe"], if: -> { last_name == "a" }
 
-#     # Will attempt to cast age to integer. Will raise an error if fails
-#     attribute :age, presence: true, type: Integer, cast: true
-#   end
-# end
+    # Will attempt to cast age to integer. Will raise an error if fails
+    attribute :age, presence: true, type: Integer, cast: true
+  end
+end
 
-# begin
-#   ExampleClass.new(first_name: "a", last_name: "b")
-# rescue Statinize::ValidationError => e
-#   e.message # => ValidationError: ...
-# end
+begin
+  ExampleClass.new(first_name: "a", last_name: "b")
+rescue Statinize::ValidationError => e
+  e.message # => ValidationError: ...
+end
 
-# example2 = ExampleClass.new(first_name: "b", last_name: "a", age: 1)
-# example2.valid? # => false
+example2 = ExampleClass.new(first_name: "b", last_name: "a", age: 1)
+example2.valid? # => false
 
-# example3 = ExampleClass.new(first_name: "a", last_name: "c", age: "1")
-# example3.valid? # => true
+example3 = ExampleClass.new(first_name: "a", last_name: "c", age: "1")
+example3.valid? # => true
 
-# # Another example
-# class AnotherExampleClass
-#   include Statinize::Statinizable
+# Another example
+class AnotherExampleClass
+  include Statinize::Statinizable
 
-#   statinize do
-#     attribute :age
-#     attribute :first_name, :last_name
-#     validate :age, type: Integer, presence: true, if: -> { first_name == "b" }
-#     validate :age, presence: true, if: [-> { first_name == "aa" }, :rubyist?], unless: -> { last_name == "Matz" }
-#   end
+  statinize do
+    attribute :age
+    attribute :first_name, :last_name
+    validate :age, type: Integer, presence: true, if: -> { first_name == "b" }
+    validate :age, presence: true, if: [-> { first_name == "aa" }, :rubyist?], unless: -> { last_name == "Matz" }
+  end
 
-#   def rubyist?
-#     last_name.include? "Matz"
-#   end
-# end
+  def rubyist?
+    last_name.include? "Matz"
+  end
+end
 
-# begin
-#   AnotherExampleClass.new(first_name: "aa", last_name: "CoolMatz")
-# rescue Statinize::ValidationError => e
-#   e.message # => ValidationError: ...
-# end
+begin
+  AnotherExampleClass.new(first_name: "aa", last_name: "CoolMatz")
+rescue Statinize::ValidationError => e
+  e.message # => ValidationError: ...
+end
 
-# example2 = AnotherExampleClass.new(first_name: "aa", last_name: "Matz") # notice no age here
-# example2.valid? # => true
+example2 = AnotherExampleClass.new(first_name: "aa", last_name: "Matz") # notice no age here
+example2.valid? # => true
 
-# begin
-#   AnotherExampleClass.new(first_name: "b", age: "1")
-# rescue Statinize::ValidationError => e
-#   e.message # => ValidationError: ...
-# end
+begin
+  AnotherExampleClass.new(first_name: "b", age: "1")
+rescue Statinize::ValidationError => e
+  e.message # => ValidationError: ...
+end
 
-# class ExampleBlockValidation
-#   include Statinize::Statinizable
+class ExampleBlockValidation
+  include Statinize::Statinizable
 
-#   statinize do
-#     force false
-#     attribute :name, :email
+  statinize do
+    force false
+    attribute :name, :email
 
-#     with type: String, presence: true do
-#       attribute :password, :encrypted_password
-#       validate :name, :email, cast: true
-#     end
+    with type: String, presence: true do
+      attribute :password, :encrypted_password
+      validate :name, :email, cast: true
+    end
 
-#     attribute :age, presence: true
+    attribute :age, presence: true
 
-#     with type: Symbol, presence: true, if: -> { age&.even? } do
-#       attribute :type, :status
-#     end
-#   end
-# end
+    with type: Symbol, presence: true, if: -> { age&.even? } do
+      attribute :type, :status
+    end
+  end
+end
 
-# # I am too lazy to include more examples for this one, but it works.
-# # Later it will all be documented
+# I am too lazy to include more examples for this one, but it works.
+# Later it will all be documented
 
-# ExampleBlockValidation.new.valid? # => false
+ExampleBlockValidation.new.valid? # => false
 
-# class ExampleStatinizeBefore
-#   include Statinize::Statinizable
+class ExampleStatinizeBeforeClass
+  include Statinize::Statinizable
 
-#   statinize do
-#     before do
-#       first_name.upcase!
-#       last_name.downcase!
-#     end
+  statinize do
+    before do
+      first_name.upcase!
+      last_name.downcase!
+    end
 
-#     attribute :first_name, :last_name, type: String
-#   end
-# end
+    attribute :first_name, :last_name, type: String
+  end
+end
 
-# e = ExampleStatinizeBefore.new(first_name: "hermes", last_name: "ATHENA")
-# e.first_name # => HERMES
-# e.last_name # => athena
+e = ExampleStatinizeBeforeClass.new(first_name: "hermes", last_name: "ATHENA")
+e.first_name # => HERMES
+e.last_name # => athena
 
-class ExampleAttributeBefore
+class ExampleAttributeBeforeClass
   include Statinize::Statinizable
 
   statinize do
@@ -113,6 +113,5 @@ class ExampleAttributeBefore
   end
 end
 
-e = ExampleAttributeBefore.new(first_name: "  Hermes  ")
-binding.pry
+e = ExampleAttributeBeforeClass.new(first_name: "  Hermes  ")
 e.first_name # => "Hermes"
