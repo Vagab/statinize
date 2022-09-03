9a857f74690dd9b0e09fb8e8c8d0e716154d264d
barseek <sergey.b@hey.com>
2022-09-03 16:07:15 +0200

    Bump version and allow string keys initialization

--- spec/statinize/initialize_with_hash_spec.rb
+++ spec/statinize/initialize_with_hash_spec.rb
@@ -1,17 +1,36 @@
 RSpec.describe "Initialize with hash" do
-  let(:params) { { name: "name", age: 69 } }
-  subject { InitializeWithHash::Example.new(params) }
+  context "symbol keys" do
+    let(:params) { { name: "name", age: 69 } }
+    subject { InitializeWithHash::Example.new(params) }
 
-  it "does not raise an error" do
-    expect { subject }.to_not raise_error
+    it "does not raise an error" do
+      expect { subject }.to_not raise_error
+    end
+
+    it "assigns name" do
+      expect(subject.name).to eq params[:name]
+    end
+
+    it "assigns age" do
+      expect(subject.age).to eq params[:age]
+    end
   end
 
-  it "assigns name" do
-    expect(subject.name).to eq params[:name]
-  end
+  context "string keys" do
+    let(:params) { { "name" => "name", "age" => 69 } }
+    subject { InitializeWithHash::Example.new(params) }
 
-  it "assigns age" do
-    expect(subject.age).to eq params[:age]
+    it "does not raise an error" do
+      expect { subject }.to_not raise_error
+    end
+
+    it "assigns name" do
+      expect(subject.name).to eq params["name"]
+    end
+
+    it "assigns age" do
+      expect(subject.age).to eq params["age"]
+    end
   end
 end
 
