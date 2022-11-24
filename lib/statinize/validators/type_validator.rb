b71a4d2bab9922e54f3b6becb6789fa6dc751bb9
barseek <sergey.b@hey.com>
2022-11-24 03:38:39 +0100

    Add proc type validation

--- lib/statinize/validators/type_validator.rb
+++ lib/statinize/validators/type_validator.rb
@@ -1,7 +1,11 @@
 module Statinize
   class TypeValidator < Validator
     def valid?
-      attr_value.is_a?(validator_value) || attr_value.nil?
+      if validator_value.is_a? Class
+        attr_value.is_a?(validator_value)
+      elsif validator_value.is_a? Proc
+        attr_value.is_a?(instance.instance_exec &validator_value)
+      end || attr_value.nil?
     end
 
     def error
