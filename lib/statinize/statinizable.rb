a74e349de45b4b142e0f89d9b3183624c88dc478
barseek <sergey.b@hey.com>
2022-11-24 03:58:55 +0100

    Proc default

--- lib/statinize/statinizable.rb
+++ lib/statinize/statinizable.rb
@@ -70,7 +70,11 @@ module Statinize
 
       def instantiate_defaults
         statinizer.attributes.select { |a| a.options.key?(:default) }.each do |attribute|
-          public_send("#{attribute.name}=", attribute.default.deep_dup)
+          if attribute.default.is_a? Proc
+            public_send("#{attribute.name}=", attribute.default.call)
+          else
+            public_send("#{attribute.name}=", attribute.default.deep_dup)
+          end
         end
       end
     end
---
+++ spec/statinize/proc_default_spec.rb
@@ -0,0 +1,32 @@
+RSpec.describe "Proc default" do
+  context "when proc default" do
+    it "sets correct default" do
+      expect(ProcDefaultDummy.new.a).to eq 1
+      expect(ProcDefaultDummy.new.a).to eq 2
+    end
+  end
+end
+
+class ProcDefaultDummy
+  include Statinize
+
+  statinize do
+    # any function which returns different things will do
+    attribute :a, default: -> { Counter.increment }
+  end
+end
+
+class Counter
+  require "singleton"
+  include Singleton
+
+  attr_accessor :counter
+
+  def initialize
+    @counter = 0
+  end
+
+  def self.increment
+    instance.counter += 1
+  end
+end
