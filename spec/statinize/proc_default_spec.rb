d64e4303a2aea3ec051a97a1bda0ca19eb7c664b
barseek <sergey.b@hey.com>
2022-11-28 12:46:15 +0100

    Fancy

--- spec/statinize/proc_default_spec.rb
+++ spec/statinize/proc_default_spec.rb
@@ -1,13 +1,19 @@
 RSpec.describe "Proc default" do
+  subject { ProcDefaultDummy.new(params) }
+
   context "when proc default" do
+    let!(:params) { {} }
+
     it "sets correct default" do
       expect(ProcDefaultDummy.new.a).to eq 1
       expect(ProcDefaultDummy.new.a).to eq 2
     end
 
     context "when proc is an instance method" do
+      let!(:params) { { default: "b" } }
+
       it "works" do
-        expect(ProcDefaultDummy.new.b).to eq "b"
+        expect(subject.b).to eq "b"
       end
     end
   end
@@ -20,9 +26,8 @@ class ProcDefaultDummy
     # any function which returns different things will do
     attribute :a, default_exec: -> { Counter.increment }
     attribute :b, default_exec: -> { default }
+    attribute :default
   end
-
-  def default = "b"
 end
 
 class Counter
